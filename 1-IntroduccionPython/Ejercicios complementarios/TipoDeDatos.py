5df8196f64ad31a6be2b8f5557713a35141b0725
Ornela04 <ocondori@fundacionedison.org.ar>
2023-08-16 19:10:36 -0300

    Ejercicios hechos

---
+++ 1-IntroduccionPython/Ejercicios complementarios/TipoDeDatos.py
@@ -0,0 +1,57 @@
+#1
+numero1 = 4
+#2
+numero2 = 10.5
+print("Los numeros son: ", numero1, " y ", numero2)
+#3
+suma = numero1 + numero2
+print("Suma: ", suma)
+#4
+resta= numero1 - numero2
+multi= numero1 * numero2
+divi= numero1 / numero2
+print("Resta: ",resta)
+print("Multiplicacion: ", multi)
+print("Division: ",divi)
+
+#5
+nombre = "ornela"
+#6
+precio = 950.99
+#7
+descuento = 0.15
+#8
+precio_final = precio -(precio*descuento)
+print("El precio ",precio, "con un 15% de descuento queda en: ", precio_final)
+
+#9
+cadena ="Esto es una cadena"
+#10
+longitud =len(cadena)
+print("Longitud de -", cadena, "- : ", longitud)
+
+#11
+precio2 = 500.9
+precio2= int(precio2)
+print("Convertir un float a entero: ", precio2)
+
+#12
+nombre = "Ornela"
+apellido = "Condori"
+nombre_completo = nombre +" "+ apellido
+print(nombre_completo)
+
+#13
+edad = 18+5-10
+print("Edad: " ,edad)
+
+#14
+altura =1.58*4/3
+print("Altura = ",altura)
+
+#15
+nombre2 = "ORNELA"
+print("Convertir mayúsculas a minusculas: ", nombre2.lower())
+
+#16
+print(nombre2[0],nombre2[1:6].lower())
