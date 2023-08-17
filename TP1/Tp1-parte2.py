aaeab88aaf235010612af14d7fa1fb5ef96ec6ce
Ornela04 <ocondori@fundacionedison.org.ar>
2023-08-17 15:51:38 -0300

    ejercicios del 1 al 6

--- TP1/Tp1-parte2.py
+++ TP1/Tp1-parte2.py
@@ -25,3 +25,27 @@ print("Divison = ", divi )
 print("Multiplicación = ", multi)
 
 #4
+fahre = float(input("Ingrese la temperatura en grados fahrenheit: "))
+cels= (fahre -32)*5/9
+print("Los grados en celcius son: ", cels)
+
+#5 
+    #a 
+nombre = input("¿Cual es tu canción favorita?")
+    #b
+precio = int(input("Precio: "))
+total = precio + (precio * 0.1)
+    #c
+edad = int(input("Edad: "))
+print("tu edad es: ", edad)
+    #d
+edad2 = int(input("Edad: "))
+edad18 = bool(edad2 == 18)
+print("Veamos si tu edad es 18: ", edad18)
+
+#6
+val1 = float(input("Ingrese el primer valor a premediar: "))
+val2 = float(input("Ingrese el segundo valor a promediar: "))
+val3 = float(input("Ingrese el tercer valor a promediar: "))
+promedio = (val1+val2+val3)/3
+print("El promedio de los números ingresados es: ", promedio)
