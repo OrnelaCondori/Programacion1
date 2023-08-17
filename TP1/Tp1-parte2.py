c07ad591684ae885f0964c5c11c40f430072690e
Ornela04 <ocondori@fundacionedison.org.ar>
2023-08-17 11:51:51 -0300

    parte2

---
+++ TP1/Tp1-parte2.py
@@ -0,0 +1,27 @@
+#1.
+base = float(input("Ingrese la base del rectangulo: "))
+altura = float(input("Ingrese la altura del rectangulo: "))
+peri = 2* base + 2*altura 
+area = base * altura
+print( "El perimetro es : ", peri)
+print("El area es: ", area)
+
+#2
+cate1 = float(input("Ingrese el cateto 1: "))
+cate2 = float(input("Ingrese el cateto 2: "))
+hipo = (cate1**2 + cate2**2)** (1/2)
+print("La hipotenusa es: ", hipo)
+
+#3.	
+num1 = float(input("Ingrese un número: "))
+num2 = float(input("Ingrese otro número: "))
+suma = num1 +num2
+resta = num1 - num2
+divi = num1 /num2
+multi = num1 * num2
+print("Suma = ", suma )
+print("Resta = ", resta )
+print("Divison = ", divi )
+print("Multiplicación = ", multi)
+
+#4
