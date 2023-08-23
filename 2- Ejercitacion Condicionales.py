f4b44b7ce89832a6acf06d5837825b093915641f
Ornela04 <ocondori@fundacionedison.org.ar>
2023-08-23 17:37:12 -0300

    terminado

--- 2- Ejercitacion Condicionales.py
+++ 2- Ejercitacion Condicionales.py
@@ -2,30 +2,36 @@ fecha = input("Ingrese día y fecha en formato  dia, DD/MM : ")
 print(fecha[0: fecha.find(",")])
 
 dia_sem = fecha[0 : fecha.find(",")]
-dia = fecha[fecha.find(" ")+1 : fecha.find("/")]
-mes = fecha[fecha.find("/")+1 : ]
+dia = int(fecha[fecha.find(" ")+1 : fecha.find("/")]) 
+mes = int(fecha[fecha.find("/")+1 : ]) 
 dia_sem = dia_sem.lower()
-print (dia_sem)
 
-if(((dia_sem !="lunes") and (dia_sem !="martes") and (dia_sem !="miercoles") and (dia_sem !="jueves") and (dia_sem !="viernes")) or( dia>32 ) or (mes >12)):
-    print("La fecha ingresada es incorrecta, vefifique que el día ingresado no sea fin de semana ni mayor a 31, o que el mes no sea mayor a 12")
+if dia_sem !="lunes" and dia_sem !="martes" and dia_sem !="miercoles" and dia_sem !="jueves" and dia_sem !="viernes" or dia>32  or mes >12:
+    print("La fecha ingresada es incorrecta")
 else:
     print("Fecha ingresada correctamente")
-    if (dia_sem == "lunes" or dia_sem=="martes" or dia_sem=="miercoles"):
-        hubo_examen = print ("¿Hubo examen? para SI ingrese 1 , para NO ingrese 2")
-        if (hubo_examen == "1"):
-            aprobados = int(input("Cuantos alumnos aprobaron: "))
-            desaprobados = int(input("Cuantos desaprobaron: "))
+    if dia_sem == "lunes" or dia_sem=="martes" or dia_sem=="miercoles":
+        hubo_examen = input("¿Hubo examen? SI/NO ")
+        hubo_examen = hubo_examen.upper()
+        if hubo_examen == "SI":
+            aprobados = int(input("Cuantos alumnos aprobaron?: "))
+            desaprobados = int(input("Cuantos desaprobaron?: "))
             porc_apro = aprobados /(aprobados+desaprobados)*100
             print(f"El porcentaje de aprobados es {porc_apro}%")
         else:
             print("No hubo examenes")
-    elif (dia_sem == "jueves"):
-        asistencia= print("Indique el procentaje de asistencia (sin porcentaje): ")
-        if(asistencia >50 ):
+    elif dia_sem == "jueves":
+        print("Hubo práctica hablada")
+        asistencia= int(input("Indique el procentaje de asistencia : %"))
+        if(asistencia > 50 ):
             print("Asistio la mayoria")
         else:
-            
+            print("no asistió la mayoría")
     else:
-        print("falta lo del viernes")
-        
+        if dia==1 and mes==1 or mes==7: 
+            print("Inglés para viajeros")
+            print("Comienzo de nuevo ciclo")
+            alumnos = int(input("Ingrese la cantidad de alumnos: "))
+            arancel = float(input("Indique el arrancel por alumno $"))
+            ingre_total = alumnos*arancel
+            print(f"El ingreso total correspondiente a {alumnos} alumnos es ${ingre_total}")
